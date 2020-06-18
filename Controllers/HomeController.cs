fec2105c9716d63fd86f9e812bc7f00ccecf8411
Alireza Mosahebi <alirezam@tpg.com.au>
2020-06-18 16:00:36 +1000

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplicationTest3.Models;
+
+namespace WebApplicationTest3.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(OperationModel model, string command)
+        {
+            if (command == "Add") {
+                model.Result = model.FirstNumber + model.SecondNumber;
+            }
+            if (command == "Subtract") {
+                model.Result = model.FirstNumber - model.SecondNumber;
+            }
+            if (command == "Muliply") {
+                model.Result = model.FirstNumber * model.SecondNumber;
+            }
+            if (command == "Divid") {
+                model.Result = model.FirstNumber / model.SecondNumber;
+            }
+            return View(model);
+        }
+
+    }
+}
---
+++ Models/OperationModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplicationTest3.Models
+{
+    public class OperationModel
+    {
+        public double FirstNumber { get; set; }
+        public double SecondNumber { get; set; }
+        public double Result { get; set; }
+    }
+}
